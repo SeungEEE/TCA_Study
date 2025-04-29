495545e9ce66cf231c403a36c366f22aea38b0d7
Seungjin Lee <sjin3687@gmail.com>
2025-04-30 00:45:51 +0900

    [Chore] 프로젝트 생성

---
+++ Standups/Standups/StandupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandupsApp.swift
+//  Standups
+//
+//  Created by 이승진 on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct StandupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Standups/StandupsTests/StandupsTests.swift
@@ -0,0 +1,17 @@
+//
+//  StandupsTests.swift
+//  StandupsTests
+//
+//  Created by 이승진 on 4/30/25.
+//
+
+import Testing
+@testable import Standups
+
+struct StandupsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
