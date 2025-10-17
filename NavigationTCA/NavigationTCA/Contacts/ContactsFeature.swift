f8154ef874731c5f075541739abbf11da5b4360a
Seunge E <sjin3687@gmail.com>
2025-10-17 17:03:22 +0900

    section1: Delete contacts
    Multiple presentation destinations

--- NavigationTCA/NavigationTCA/Contacts/ContactsFeature.swift
+++ NavigationTCA/NavigationTCA/Contacts/ContactsFeature.swift
@@ -18,11 +18,17 @@ struct ContactsFeature {
   @ObservableState
   struct State: Equatable {
     @Presents var addContact: AddContactFeature.State?
+    @Presents var alert: AlertState<Action.Alert>?
     var contacts: IdentifiedArrayOf<Contact> = []
   }
   enum Action {
     case addButtonTapped
     case addContact(PresentationAction<AddContactFeature.Action>)
+    case alert(PresentationAction<Alert>)
+    case deleteButtonTapped(id: Contact.ID)
+    enum Alert: Equatable {
+      case confirmDeletion(id: Contact.ID)
+    }
   }
   var body: some ReducerOf<Self> {
     Reduce { state, action in
@@ -33,16 +39,28 @@ struct ContactsFeature {
         )
         return .none
         
-//      case .addContact(.presented(.delegate(.cancel))):
-//        state.addContact = nil
-//        return .none
-        
       case let .addContact(.presented(.delegate(.saveContact(contact)))):
         state.contacts.append(contact)
-//        state.addContact = nil
         return .none
         
       case .addContact:
+        return .none
+        
+      case let .alert(.presented(.confirmDeletion(id: id))):
+        state.contacts.remove(id: id)
+        return .none
+        
+      case .alert:
+        return .none
+        
+      case let .deleteButtonTapped(id: id):
+        state.alert = AlertState {
+          TextState("Are you sure?")
+        } actions: {
+          ButtonState(role: .destructive, action: .confirmDeletion(id: id)) {
+            TextState("Delete")
+          }
+        }
         return .none
       }
     }
@@ -50,5 +68,6 @@ struct ContactsFeature {
     .ifLet(\.$addContact, action: \.addContact) {
       AddContactFeature()
     }
+    .ifLet(\.$alert, action: \.alert)
   }
 }
