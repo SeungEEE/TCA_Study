569b3b85b0b57940ff34d24289d36532e4c8a949
Seunge E <sjin3687@gmail.com>
2025-10-17 16:19:09 +0900

    chore: 프로젝트 생성

---
+++ NavigationTCA/NavigationTCA/NavigationTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationTCAApp.swift
+//  NavigationTCA
+//
+//  Created by 이안 on 10/17/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
